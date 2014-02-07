774c1537a0f1a9d8b3bce3e91ed40512a8d071ee
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-02-06 16:43:44 -0800

    Added basic grunt compilation

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-bumpup');
+    grunt.loadNpmTasks('grunt-tagrelease');
+
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            js: {
+                files: ['src/scripts/**/*.js'],
+                tasks: ['concat', 'uglify']
+            }
+        },
+
+        bumpup: {
+            options: {
+                updateProps: {
+                    pkg: 'package.json'
+                }
+            },
+            files: ['package.json']
+        },
+
+        tagrelease: '<%= pkg.version %>',
+
+        ver: {
+            files: ['src/scripts/version.js']
+        },
+
+        scripts: {
+            libs: [
+                'src/scripts/utils.js'
+            ],
+            connectors: [
+                'src/scripts/connector-base.js',
+                'src/scripts/**/*.js'
+            ]
+
+        },
+        concat: {
+            all: {
+                src: ['src/scripts/header.js', '<%= scripts.connectors %>', 'src/scripts/footer.js'],
+                dest: 'dist/narwhal.connectors.js'
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: function (fileName) {
+                    return fileName.replace(/\.js$/, '.map');
+                },
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            production: {
+                files: {
+                    'dist/narwhal.connectors.min.js': ['dist/narwhal.connectors.js']
+                }
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('linked', ['concat', 'uglify', 'less:uncompressed', 'less:production', 'watch']);
+    grunt.registerTask('default', ['less:dev', 'watch:less']);
+    grunt.registerTask('production', function (type) {
+        type = type ? type : 'patch';
+        ['bumpup:' + type, 'ver', 'concat', 'uglify', 'less:production', 'tagrelease'].forEach(function (task) {
+            grunt.task.run(task);
+        });
+    });
+
+
+    grunt.registerMultiTask('ver', 'Update version file with what\'s in package.json', function () {
+        var pkg = require('./package.json');
+        this.data.forEach(function (f) {
+            console.log('Updating ' + f + ' to version ' + pkg.version);
+            grunt.file.write(f, "Narwhal.connectors.version = '" + pkg.version + "';");
+        });
+    });
+
+};
