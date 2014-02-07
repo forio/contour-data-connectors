b93a8ba5f6b001634bd62b055da2ed5c67e1dea6
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-02-06 17:06:54 -0800

    Moved data manipulation functionality to base class

--- src/scripts/connector-base.js
+++ src/scripts/connector-base.js
@@ -4,9 +4,93 @@
 
     ConnectorBase.prototype = {
         initialize: function () {
+        },
+
+        dimension: function (_) {
+            if(!arguments.length) return this._headers[this._dimension];
+            this._dimension = this._headers.indexOf(_.toLowerCase());
+            return this;
+        },
+
+        filter: function (selector) {
+            this._filterSelector = selector;
+            return this;
+        },
+
+        newMeasure: function (name, fn) {
+
+            this._newMeasure = {
+                name: name,
+                fn: fn,
+                index: this._headers.length
+            };
+
+            return this;
+        },
+
+        rollup: function (selector) {
+            this._rollup = selector;
+            return this;
+        },
+
+        measure: function (name) {
+            name = _.isArray(name) ? name : [name];
+
+            return this.data(_.map(name, String.toLowerCase));
+        },
+
+        top: function (t) {
+            this._take = t;
+            return this;
+        },
+
+        bottom: function (t) {
+            this._take = -t;
+            return this;
+        },
+
+        data: function (measures) {
+            measures = _.isArray(measures) ? measures : [measures];
+            var _this = this;
+            var filteredData = this._filterSelector ? _.filter(_this._data, function(row, index) { return _this._filterSelector.call(_this, row, index, _this._headers); }) : this._data;
+            var rolledRight = this._newMeasure ? _.map(filteredData, function (row, index) { row.push(_this._newMeasure.fn.call(_this, row, index, _this._headers)); return row; } ) : filteredData;
+            var sortMeasureIndex = this._getMeasureIndex(measures[0]);
+
+            if (this._take) {
+                rolledRight.sort(function (a, b) {
+                    // console.log();
+                    return +b[sortMeasureIndex] - +a[sortMeasureIndex];
+                });
+                rolledRight = this._take > 0 ? rolledRight.slice(0, this._take) : rolledRight.slice(this._take);
+            }
+
+            var result = _.map(measures, function (m) { return _this._generateSeries.call(_this, m, rolledRight); });
+
+            return result;
+        },
+
+        _getMeasureIndex: function (measure) {
+            return this._newMeasure && this._newMeasure.name === measure ? this._newMeasure.index : this._headers.indexOf(measure);
+
+        },
+
+        _generateSeries: function (measure, data) {
+            var dimIndex = this._dimension;
+            var measureIndex = this._getMeasureIndex(measure);
+            var result = _.map(data, function (d) {
+                return {
+                    x: d[dimIndex],
+                    y: _.isNaN(+d[measureIndex]) ? d[measureIndex] : +d[measureIndex]
+                };
+            });
+
+            return { name: measure, data: result };
         }
     };
 
     ConnectorBase.extend = Narwhal.extend;
+    Narwhal = Narwhal || {};
+    Narwhal.connectors = Narwhal.connectors || {};
+    Narwhal.connectors.ConnectorBase = ConnectorBase;
 
 })();
--- src/scripts/csv.js
+++ src/scripts/csv.js
@@ -1,49 +1,28 @@
 (function () {
 
-    function Csv(raw, headerRow) {
-        headerRow = typeof headerRow === 'undefined' ? true : headerRow;
-        this.parse(raw, headerRow);
+    var Csv = Narwhal.connectors.ConnectorBase.extend({
+        constructor: function (raw, headerRow) {
+            headerRow = typeof headerRow === 'undefined' ? true : headerRow;
+            this.parse(raw, headerRow);
 
-        this._dimension = 0;
+            this._dimension = 0;
 
-        return this;
-    }
+            return this;
+        },
 
-    Csv.prototype = {
+        splitPatter: /\t/,
+
         parse: function (raw, headerRow) {
             this._data = [];
             this._headers = [];
             if (!raw || !raw.length) return ;
-            var rows = raw.split(/\n\r?/);
-            this._headers = headerRow ? _.each(rows.shift().split(','), function(d) { return d.toLowerCase(); }) : _.range(0, rows[0].length);
+            var rows = raw.split(/\r\n|\r|\n/);
+            this._headers = headerRow ? _.each(rows.shift().split(this.splitPatter), function(d) { return d.toLowerCase(); }) : _.range(0, rows[0].length);
             _.each(rows, function (r) {
-                this._data.push(r.split(','));
+                this._data.push(r.split(this.splitPatter));
             }, this);
-        },
-
-        dimension: function (_) {
-            if(!arguments.length) return this._headers[this._dimension];
-            this._dimension = this._headers.indexOf(_.toLowerCase());
-            return this;
-        },
-
-        measure: function (_) {
-            return this.data(_.toLowerCase());
-        },
-
-        data: function (measure) {
-            var dimIndex = this._dimension;
-            var measureIndex = this._headers.indexOf(measure);
-            var result = _.map(this._data, function (d) {
-                return {
-                    x: d[dimIndex],
-                    y: _.isNaN(+d[measureIndex]) ? d[measureIndex] : +d[measureIndex]
-                };
-            });
-
-            return result;
         }
-    };
+    });
 
     Narwhal.connectors = Narwhal.connectors || {};
     Narwhal.connectors.Csv = Csv;
