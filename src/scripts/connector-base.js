d9afe31eb346e186c79e16118e0b8634a37049e0
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-02-10 16:51:57 -0800

    fixed agregation of dimension data in base connector

--- src/scripts/connector-base.js
+++ src/scripts/connector-base.js
@@ -69,9 +69,42 @@
             return this;
         },
 
-        rollup: function (selector) {
-            this._rollup = selector;
-            return this;
+        _rollup: function (data, dimension) {
+            var rows = {};
+            var dimensionIndex = dimension;
+            var hashEntry;
+
+            // map
+            _.each(data, function (d) {
+                if (!(hashEntry = rows[d[dimensionIndex]])) {
+                    rows[d[dimensionIndex]] = hashEntry = [];
+                }
+
+                hashEntry.push(d);
+            });
+
+            // reduce
+            var reduced = [];
+            var gr = [];
+
+            var reducer = function (row) {
+                _.each(row, function (entry, i) {
+                    if (!isNaN(+entry)) {
+                        var val = +entry;
+                        gr[i] = (gr[i] || 0) + val;
+                    } else {
+                        gr[i] = gr[i] || entry;
+                    }
+                });
+            };
+
+            for (var key in rows) {
+                gr = [];
+                _.each(rows[key], reducer);
+                reduced.push(gr);
+            }
+
+            return reduced;
         },
 
         measure: function (name) {
@@ -95,20 +128,25 @@
             var _this = this;
             var filteredData = this._filterSelector ? _.filter(_this._data, function(row, index) { return _this._filterSelector.call(_this, row, index, _this._headers); }) : this._data;
             var rolledRight = this._newMeasure ? _.map(filteredData, function (row, index) { var r = row.slice(); r.push(_this._newMeasure.fn.call(_this, row, index, _this._headers)); return r; } ) : filteredData;
+            var rolledUp = this._rollup(rolledRight, this._dimension);
             var sortMeasureIndex = this._getMeasureIndex(measures[0]);
 
             if (this._take) {
-                rolledRight.sort(function (a, b) { return +b[sortMeasureIndex] - +a[sortMeasureIndex]; });
-                rolledRight = this._take > 0 ? rolledRight.slice(0, this._take) : rolledRight.slice(this._take);
+                rolledUp.sort(function (a, b) { return +b[sortMeasureIndex] - +a[sortMeasureIndex]; });
+                rolledUp = this._take > 0 ? rolledUp.slice(0, this._take) : rolledUp.slice(this._take);
             }
 
-            var result = _.map(measures, function (m) { return _this._generateSeries.call(_this, m, rolledRight); });
+            var result = _.map(measures, function (m) { return _this._generateSeries.call(_this, m, rolledUp); });
 
             return result;
         },
 
         _getMeasureIndex: function (measure) {
             return this._newMeasure && this._newMeasure.name === measure ? this._newMeasure.index : this._headers.indexOf(measure);
+        },
+
+        _getDimensionIndex: function (dimension) {
+            return this._headers.indexOf(dimension);
         },
 
         _generateSeries: function (measure, data) {
