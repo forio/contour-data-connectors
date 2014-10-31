0e077d57851a627a67b91ae1afe0134621124e10
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-10-30 23:21:15 -0700

    updated version file to 0.9.106

--- dist/contour.connectors.js
+++ dist/contour.connectors.js
@@ -139,6 +139,7 @@
         * Specifies the dimension to be used when passing this data set to a Contour visualization. Dimensions are any non-numeric data.
         *
         * ### Example:
+        *
         *       var csvData = 'quarter,region,cost,revenue,profit\n2013Q1,North,100,150,50\n2013Q1,South,200,250,50\n2013Q2,North,110,150,40\n2013Q2,South,220,250,30\n2013Q3,North,90,180,90\n2013Q3,South,115,180,65\n2013Q4,North,105,190,85\n2013Q4,South,90,180,90';
         *       var csv = new Contour.connectors.Csv(csvData);
         *
@@ -302,6 +303,7 @@
         * Returns only the top `t` results from the sorted data set. Call this after you have specified dimensions and/or filters but before you have called `.measure()`.
         *
         * ### Example:
+        *
         *       var csvData = 'quarter,region,cost,revenue,profit\n2013Q1,North,100,150,50\n2013Q1,South,200,250,50\n2013Q2,North,110,150,40\n2013Q2,South,220,250,30\n2013Q3,North,90,180,90\n2013Q3,South,115,180,65\n2013Q4,North,105,190,85\n2013Q4,South,90,180,90';
         *       var csv = new Contour.connectors.Csv(csvData);
         *
@@ -328,6 +330,7 @@
         * Returns only the bottom `t` results from the sorted data set. Call this after you have specified dimensions and/or filters but before you have called `.measure()`.
         *
         * ### Example:
+        *
         *       var csvData = 'quarter,region,cost,revenue,profit\n2013Q1,North,100,150,50\n2013Q1,South,200,250,50\n2013Q2,North,110,150,40\n2013Q2,South,220,250,30\n2013Q3,North,90,180,90\n2013Q3,South,115,180,65\n2013Q4,North,105,190,85\n2013Q4,South,90,180,90';
         *       var csv = new Contour.connectors.Csv(csvData);
         *
@@ -554,4 +557,4 @@
 
 })();
 
-Contour.connectors.version = '0.9.105';
+Contour.connectors.version = '0.9.106';
--- src/scripts/version.js
+++ src/scripts/version.js
@@ -1,1 +1,1 @@
-Contour.connectors.version = '0.9.105';
+Contour.connectors.version = '0.9.106';
